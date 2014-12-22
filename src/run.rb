e5dfd60d792db740b22b6e1abdbe7392a68d17d8
Outsider <outsideris@gmail.com>
2014-12-23 01:24:15 +0900

    부산대 IP 수정

--- src/run.rb
+++ src/run.rb
@@ -9,7 +9,7 @@ query_file = File.join home_dir, 'query'
 query = File.read query_file
 
 # 부산대 맞춤법/문법 검사기 접속
-uri = URI.parse 'http://speller.cs.pusan.ac.kr/PnuSpellerISAPI_201309/lib/PnuSpellerISAPI_201309.dll?Check'
+uri = URI.parse 'http://164.125.36.75/PnuSpellerISAPI_201309/lib/PnuSpellerISAPI_201309.dll?Check'
 
 http = Net::HTTP.new uri.host, uri.port
 
